018aa60451837dd5ae4016c8b0dffb89c5fa7458
semigura <shotaro-ishigaki@gaji.jp>
2021-12-18 19:11:51 +0900

    Add Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Layout = styled.div`
+  padding: 8px;
+  background-color: #000;
+  color: #bbb;
+  font-size: 14px;
+  line-height: 1.5;
+  min-height: 100vh;
+`;
