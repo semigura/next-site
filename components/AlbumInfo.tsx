9808a7bbb85f1b5698eaefc6ff9ab02d02078fbb
semigura <shotaro-ishigaki@gaji.jp>
2021-12-21 23:55:54 +0900

    Add AlbumInfo

---
+++ components/AlbumInfo.tsx
@@ -0,0 +1,47 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { ReactNode } from "react";
+
+export const AlbumInfoDiv = styled.div`
+  margin-bottom: calc(2em * 1.5);
+`;
+
+interface Props {
+  label?: ReactNode;
+  tracklisting: ReactNode;
+  streamings?: ReactNode;
+  note?: ReactNode;
+}
+
+export const AlbumInfo = ({ label, tracklisting, streamings, note }: Props) => {
+  return (
+    <AlbumInfoDiv>
+      {label && (
+        <div
+          css={css`
+            margin-bottom: calc(1em * 1.5);
+          `}
+        >
+          {label}
+        </div>
+      )}
+      <div
+        css={css`
+          margin-bottom: calc(1em * 1.5);
+        `}
+      >
+        {tracklisting}
+      </div>
+      {streamings && (
+        <div
+          css={css`
+            margin-bottom: calc(1em * 1.5);
+          `}
+        >
+          {streamings}
+        </div>
+      )}
+      {note && note}
+    </AlbumInfoDiv>
+  );
+};
